442e6a44be6260999b3bc390699cc22f1ce2f9fd
Heinz A Preisig <heinz.preisig@chemeng.ntnu.no>
2020-08-07 18:15:27 +0200

    fixing intraface definition in equation editory

--- tree_experiment.py
+++ tree_experiment.py
@@ -8,12 +8,15 @@ from Common.ontology_container import OntologyContainer
 from OntologyBuilder.OntologyEquationEditor.resources import DotGraph
 
 if __name__ == '__main__':
-  ontology_name = "HAP_playground_02_extend_ontology"
+  ontology_name = "HAP_Ontology_Repository-playground_v8"
 
-  ontology_containere = OntologyContainer(ontology_name)
-  variables = ontology_containere.vars
-  indices = ontology_containere.indices
+
+  ontology_container = OntologyContainer(ontology_name)
+  variables = ontology_container.vars
+  indices = ontology_container.indices
 
   # make_variable_equation_pngs(variables, ontology_name)
 
   dot = DotGraph(variables, indices, 10, ontology_name)
+
+  print("finished")
